36a03b0b707ef37dcd8c519680358f2f714c0147
Raghavv1206 <raghavm205@gmail.com>
2026-06-25 16:49:42 +0530

    twenth commit

--- bhel_connect_backend/settings.py
+++ bhel_connect_backend/settings.py
@@ -163,6 +163,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
 }
 
 # Auto-fallback to local filesystem storage if Cloudinary is not configured or uses placeholders
